82b38de1f2a51b433c8fb4cc27c8d2dc1425c3e6
raselrostock <raselrostock@gmail.com>
2019-06-04 12:53:26 +0200

    Added profile component

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -2,17 +2,20 @@
   <div class="navbar">
     <nav class="deep-purple darken-1">
       <div class="container">
-        <a href class="brand-logo">
+        <a href class="brand-logo left">
           <router-link :to="{ name: 'Map' }">GeoApp</router-link>
         </a>
         <ul class="right">
-          <li>
+          <li v-if="!user">
             <router-link :to="{ name: 'Signup' }">Signup</router-link>
           </li>
-          <li>
+          <li v-if="!user">
             <router-link :to="{ name: 'Login' }">Login</router-link>
           </li>
-          <li>
+          <li v-if="user">
+            <a>{{ user.email }}</a>
+          </li>
+          <li v-if="user">
             <a @click="logOut">Logout</a>
           </li>
         </ul>
@@ -25,7 +28,9 @@ import firebase from "firebase";
 export default {
   name: "Navbar",
   data() {
-    return {};
+    return {
+      user: null
+    };
   },
   methods: {
     logOut() {
@@ -36,6 +41,16 @@ export default {
           this.$router.push({ name: "Login" });
         });
     }
+  },
+  created() {
+    //let user = firebase.auth().currentUser
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.user = user;
+      } else {
+        this.user = null;
+      }
+    });
   }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,20 @@ import Router from 'vue-router';
 import Map from '@/components/home/Map';
 import Signup from '@/components/auth/Signup';
 import Login from '@/components/auth/Login';
-
+import ViewProfile from '@/components/profile/ViewProfile';
+import firebase from 'firebase';
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   // mode: history,
   routes: [
     {
       path: '/',
       name: 'Map',
-      component: Map
+      component: Map,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/signup',
@@ -23,6 +27,33 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/profile/:id',
+      name: 'ViewProfile',
+      component: ViewProfile,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  //Check to see if the route requires auth
+  if (to.matched.some(rec => rec.meta.requiresAuth)) {
+    // check auth state of user
+    let user = firebase.auth().currentUser;
+    if (user) {
+      // user signed in, proceed to route
+      next();
+    } else {
+      // no user signed in, redirect to login
+      next({ name: 'Login' });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
